ad231a4e5412a3e6018073946f8ac31885fb1519
shashkambh <shashankk@utexas.edu>
2016-10-07 20:13:58 -0500

    Initial commit

---
+++ src/assignment4/Algae.java
@@ -0,0 +1,16 @@
+package assignment4;
+/*
+ * Do not change this file.
+ */
+import assignment4.Critter.TestCritter;
+
+public class Algae extends TestCritter {
+
+	public String toString() { return "@"; }
+	
+	public boolean fight(String not_used) { return false; }
+	
+	public void doTimeStep() {
+		setEnergy(getEnergy() + Params.photosynthesis_energy_amount);
+	}
+}
---
+++ src/assignment4/Header.java
@@ -0,0 +1,13 @@
+package assignment4;
+/* CRITTERS <MyClass.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Fall 2016
+ */
